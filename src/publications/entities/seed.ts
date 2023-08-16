50588a654d583d6c7ddf7f3c46f33ac4fc1c4dfa
Sebastian Mosquera <sebas19957@hotmail.com>
2023-08-16 01:14:31 -0500

    Prepare the data to create the seed in for each of the entities, the data of the information of the entity friends is missing

---
+++ src/publications/entities/seed.ts
@@ -0,0 +1,266 @@
+//TODO: cambiarle el id a todos los comentarios para que sean únicos
+
+interface SeedPublications {
+  id: string;
+  user_id: string;
+  content: string;
+}
+
+interface SeedDataPublications {
+  publications: SeedPublications[];
+}
+
+export const initalDataPublications: SeedDataPublications = {
+  publications: [
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      content: 'Innovar o desaparecer.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'a1b2c3d4-e5f6-g7h8-i9j0-k1l2m3n4o5p',
+      content: 'Piensa diferente.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'q1w2e3r4-t5y6-u7i8-o9p0-a1s2d3f4g5h',
+      content: 'Menos es más.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'j1k2l3m4-n5b6-v7c8-x9z0-q1w2e3r4t5y',
+      content: 'Conectar el mundo.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'u1i2o3p4-a5s6-d7f8-g9h0-j1k2l3m4n5b',
+      content: 'La tecnología transforma.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'c5v6b7n8-m9q0-w1e2-r3t4-y5u6i7o8p9a',
+      content: 'Simplifica la complejidad.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 's5d6f7g8-h9j0-k1l2-z3x4-c5v6b7n8m9q',
+      content: 'Código y creación.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'w5e6r7t8-y9u0-i1o2-p3a4-s5d6f7g8h9j',
+      content: 'Aprende, adapta, evoluciona.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'k5l6z7x8-c9v0-b1n2-m3q4-w5e6r7t8y9u',
+      content: 'La imaginación es el límite.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'i5o6p7a8-s9d0-f1g2-h3j4-k5l6z7x8c9v',
+      content: 'Crear el futuro.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'b5n6m7q8-w9e0-r1t2-y3u4-i5o6p7a8s9d',
+      content: 'La máquina es neutra.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'f5g6h7j8-k9l0-z1x2-c3v4-b5n6m7q8w9e',
+      content: 'Bits y átomos.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'r5t6y7u8-i9o0-p1a2-s3d4-f5g6h7j8k9l',
+      content: 'Programa tu mundo.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'v5b6n7m8-q9w0-e1r2-t3y4-u5i6o7p8a9s',
+      content: 'Virtualidad real.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'd5f6g7h8-j9k0-l1z2-x3c4-v5b6n7m8q9w',
+      content: 'Construye con código.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'e5r6t7y8-u9i0-o1p2-a3s4-d5f6g7h8j9k',
+      content: 'La tecnología humaniza.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'l5z6x7c8-v9b0-n1m2-q3w4-e5r6t7y8u9i',
+      content: 'Datos, la nueva moneda.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'p5a6s7d8-f9g0-h1j2-k3l4-z5x6c7v8b9n',
+      content: 'Hazlo posible.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'm5q6w7e8-r9t0-y1u2-i3o4-p5a6s7d8f9g',
+      content: 'Automatiza y prospera.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'h5j6k7l8-z9x0-c1v2-b3n4-m5q6w7e8r9t',
+      content: 'Internet: la red global.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'y5u6i7o8-p9a0-s1d2-f3g4-h5j6k7l8z9x',
+      content: 'Crea el cambio.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'c5v6b7n8m9-q0w1-e2r3-t4y5-u6i7o8p9a0s',
+      content: 'Diseña el mañana.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'd5f6g7h8j9-k0l1-z2x3-c4v5-b6n7m8q9w0',
+      content: 'Velocidad y progreso.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'e5r6t7y8u9-i0o1-p2a3-s4d5-f6g7h8j9k0l',
+      content: 'Conocimiento en línea.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'q5w6e7r8t9-y0u1-i2o3-p4a5-s6d7f8g9h0j',
+      content: 'Empodera con tecnología.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'k5l6z7x8c9-v0b1-n2m3-q4w5-e6r7t8y9u0i',
+      content: 'Conectados siempre.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'o5p6a7s8-d9f0-g1h2-j3k4-l5z6x7c8v9b0n',
+      content: 'Algoritmos que guían.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'm5q6w7e8r9-t0y1-u2i3-o4p5-a6s7d8f9g0h',
+      content: 'Tecnología = Oportunidad.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'g5h6j7k8-l9z0-x1c2-v3b4-n5m6-q7w8e9r0t',
+      content: 'Herramienta de posibilidades.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'y5u6i7o8-p9a0-s1d2-f3g4-h5j6k7l8z9x',
+      content: 'Realidad virtual, futuro presente.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'c5v6b7n8m9-q0w1-e2r3-t4y5-u6i7o8p9a0s',
+      content: 'Máquinas y humanos.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'd5f6g7h8j9-k0l1-z2x3-c4v5-b6n7m8q9w0',
+      content: 'Diseño y función.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'e5r6t7y8u9-i0o1-p2a3-s4d5-f6g7h8j9k0l',
+      content: 'Redes cambian todo.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'q5w6e7r8t9-y0u1-i2o3-p4a5-s6d7f8g9h0j',
+      content: 'Aprende, desaprende, reaprende.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'k5l6z7x8c9-v0b1-n2m3-q4w5-e6r7t8y9u0i',
+      content: 'Digitaliza tu vida.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'o5p6a7s8-d9f0-g1h2-j3k4-l5z6x7c8v9b0n',
+      content: 'Sociedad digitalizada.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'm5q6w7e8r9-t0y1-u2i3-o4p5-a6s7d8f9g0h',
+      content: 'Inteligencia artificial: futuro hoy.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'g5h6j7k8-l9z0-x1c2-v3b4-n5m6-q7w8e9r0t',
+      content: 'Información, poder distribuido.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'y5u6i7o8-p9a0-s1d2-f3g4-h5j6k7l8z9x',
+      content: 'El futuro es ahora.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'c5v6b7n8m9-q0w1-e2r3-t4y5-u6i7o8p9a0s',
+      content: 'Tecnología, motor del cambio.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'd5f6g7h8j9-k0l1-z2x3-c4v5-b6n7m8q9w0',
+      content: 'Mentes electrónicas.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'e5r6t7y8u9-i0o1-p2a3-s4d5-f6g7h8j9k0l',
+      content: 'Algoritmos, decisiones.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'q5w6e7r8t9-y0u1-i2o3-p4a5-s6d7f8g9h0j',
+      content: 'Software rige el mundo.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'k5l6z7x8c9-v0b1-n2m3-q4w5-e6r7t8y9u0i',
+      content: 'Evolución digital.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'o5p6a7s8-d9f0-g1h2-j3k4-l5z6x7c8v9b0n',
+      content: 'Mañana se inventa hoy.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'm5q6w7e8r9-t0y1-u2i3-o4p5-a6s7d8f9g0h',
+      content: 'Codifica el mañana.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'g5h6j7k8-l9z0-x1c2-v3b4-n5m6-q7w8e9r0t',
+      content: 'Tecnología: maravilla constante.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'y5u6i7o8-p9a0-s1d2-f3g4-h5j6k7l8z9x',
+      content: 'Humanidad digital.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'c5v6b7n8m9-q0w1-e2r3-t4y5-u6i7o8p9a0s',
+      content: 'Digitaliza tu pasión.',
+    },
+    {
+      id: 'd8e8e3e1-7e4a-4e4b-8e4c-9e4d8e4e8e4f',
+      user_id: 'd5f6g7h8j9-k0l1-z2x3-c4v5-b6n7m8q9w0',
+      content: 'Adapta o obsoleto.',
+    },
+  ],
+};
