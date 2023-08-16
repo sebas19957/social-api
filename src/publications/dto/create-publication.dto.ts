8acd871087c33c570405bdcda12ed006112b73fc
Analista 1 <analista1@MERCADEOVIRTUAL.local>
2023-08-16 17:27:43 -0500

    Create the relationships between entities and create the resource to create the seeds

--- src/publications/dto/create-publication.dto.ts
+++ src/publications/dto/create-publication.dto.ts
@@ -1,1 +1,9 @@
-export class CreatePublicationDto {}
+import { IsString, IsUUID } from 'class-validator';
+
+export class CreatePublicationDto {
+  @IsUUID(undefined, { message: 'El campo user_id debe ser un UUID válido' })
+  user_id: string;
+
+  @IsString({ message: 'El campo nombre debe ser una cadena de texto' })
+  content: string;
+}
--- src/publications/publications.module.ts
+++ src/publications/publications.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PublicationsService } from './publications.service';
 import { PublicationsController } from './publications.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Publication } from './entities/publication.entity';
 
 @Module({
   controllers: [PublicationsController],
   providers: [PublicationsService],
+  imports: [TypeOrmModule.forFeature([Publication])],
 })
 export class PublicationsModule {}
